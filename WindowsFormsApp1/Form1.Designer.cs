238e42d6eb05c5a5612002e9f0d86a3639159c66
Matěj Iljuchin <loupeznik@gmail.com>
2024-05-06 20:52:52 +0200

    UI elements added

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,13 +28,160 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Load_Button = new System.Windows.Forms.Button();
+            this.Save_Button = new System.Windows.Forms.Button();
+            this.Pen_Tool_Button = new System.Windows.Forms.Button();
+            this.Line_Tool_Button = new System.Windows.Forms.Button();
+            this.Rectangle_Tool_Button = new System.Windows.Forms.Button();
+            this.Circle_Tool_Button = new System.Windows.Forms.Button();
+            this.Ellipse_Tool_Button = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(776, 426);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Load_Button
+            // 
+            this.Load_Button.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.Load_Button.FlatAppearance.BorderSize = 3;
+            this.Load_Button.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.Load_Button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.Load_Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Load_Button.Location = new System.Drawing.Point(39, 27);
+            this.Load_Button.Name = "Load_Button";
+            this.Load_Button.Size = new System.Drawing.Size(55, 46);
+            this.Load_Button.TabIndex = 1;
+            this.Load_Button.Text = "Load";
+            this.Load_Button.UseVisualStyleBackColor = true;
+            // 
+            // Save_Button
+            // 
+            this.Save_Button.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.Save_Button.FlatAppearance.BorderSize = 3;
+            this.Save_Button.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.Save_Button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.Save_Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Save_Button.Location = new System.Drawing.Point(100, 27);
+            this.Save_Button.Name = "Save_Button";
+            this.Save_Button.Size = new System.Drawing.Size(55, 46);
+            this.Save_Button.TabIndex = 2;
+            this.Save_Button.Text = "Save";
+            this.Save_Button.UseVisualStyleBackColor = true;
+            // 
+            // Pen_Tool_Button
+            // 
+            this.Pen_Tool_Button.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.Pen_Tool_Button.FlatAppearance.BorderSize = 3;
+            this.Pen_Tool_Button.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.Pen_Tool_Button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.Pen_Tool_Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Pen_Tool_Button.Location = new System.Drawing.Point(161, 27);
+            this.Pen_Tool_Button.Name = "Pen_Tool_Button";
+            this.Pen_Tool_Button.Size = new System.Drawing.Size(55, 46);
+            this.Pen_Tool_Button.TabIndex = 3;
+            this.Pen_Tool_Button.Text = "Pen";
+            this.Pen_Tool_Button.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Pen_Tool_Button.UseVisualStyleBackColor = true;
+            // 
+            // Line_Tool_Button
+            // 
+            this.Line_Tool_Button.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.Line_Tool_Button.FlatAppearance.BorderSize = 3;
+            this.Line_Tool_Button.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.Line_Tool_Button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.Line_Tool_Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Line_Tool_Button.Location = new System.Drawing.Point(222, 27);
+            this.Line_Tool_Button.Name = "Line_Tool_Button";
+            this.Line_Tool_Button.Size = new System.Drawing.Size(55, 46);
+            this.Line_Tool_Button.TabIndex = 4;
+            this.Line_Tool_Button.Text = "Line";
+            this.Line_Tool_Button.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Line_Tool_Button.UseVisualStyleBackColor = true;
+            // 
+            // Rectangle_Tool_Button
+            // 
+            this.Rectangle_Tool_Button.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.Rectangle_Tool_Button.FlatAppearance.BorderSize = 3;
+            this.Rectangle_Tool_Button.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.Rectangle_Tool_Button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.Rectangle_Tool_Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Rectangle_Tool_Button.Location = new System.Drawing.Point(283, 27);
+            this.Rectangle_Tool_Button.Name = "Rectangle_Tool_Button";
+            this.Rectangle_Tool_Button.Size = new System.Drawing.Size(55, 46);
+            this.Rectangle_Tool_Button.TabIndex = 5;
+            this.Rectangle_Tool_Button.Text = "Rectangle";
+            this.Rectangle_Tool_Button.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Rectangle_Tool_Button.UseVisualStyleBackColor = true;
+            // 
+            // Circle_Tool_Button
+            // 
+            this.Circle_Tool_Button.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.Circle_Tool_Button.FlatAppearance.BorderSize = 3;
+            this.Circle_Tool_Button.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.Circle_Tool_Button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.Circle_Tool_Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Circle_Tool_Button.Location = new System.Drawing.Point(344, 27);
+            this.Circle_Tool_Button.Name = "Circle_Tool_Button";
+            this.Circle_Tool_Button.Size = new System.Drawing.Size(55, 46);
+            this.Circle_Tool_Button.TabIndex = 6;
+            this.Circle_Tool_Button.Text = "Circle";
+            this.Circle_Tool_Button.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Circle_Tool_Button.UseVisualStyleBackColor = true;
+            // 
+            // Ellipse_Tool_Button
+            // 
+            this.Ellipse_Tool_Button.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.Ellipse_Tool_Button.FlatAppearance.BorderSize = 3;
+            this.Ellipse_Tool_Button.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.Ellipse_Tool_Button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.Ellipse_Tool_Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Ellipse_Tool_Button.Location = new System.Drawing.Point(405, 27);
+            this.Ellipse_Tool_Button.Name = "Ellipse_Tool_Button";
+            this.Ellipse_Tool_Button.Size = new System.Drawing.Size(55, 46);
+            this.Ellipse_Tool_Button.TabIndex = 7;
+            this.Ellipse_Tool_Button.Text = "Ellipse";
+            this.Ellipse_Tool_Button.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Ellipse_Tool_Button.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.Ellipse_Tool_Button);
+            this.Controls.Add(this.Circle_Tool_Button);
+            this.Controls.Add(this.Rectangle_Tool_Button);
+            this.Controls.Add(this.Line_Tool_Button);
+            this.Controls.Add(this.Pen_Tool_Button);
+            this.Controls.Add(this.Save_Button);
+            this.Controls.Add(this.Load_Button);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "PaintSuper";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Load_Button;
+        private System.Windows.Forms.Button Save_Button;
+        private System.Windows.Forms.Button Pen_Tool_Button;
+        private System.Windows.Forms.Button Line_Tool_Button;
+        private System.Windows.Forms.Button Rectangle_Tool_Button;
+        private System.Windows.Forms.Button Circle_Tool_Button;
+        private System.Windows.Forms.Button Ellipse_Tool_Button;
     }
 }
 

1e7e99476f676e1b4ed8c8804138b189716fa6b8
Matěj Iljuchin <loupeznik@gmail.com>
2024-05-06 20:56:14 +0200

    changed names

--- WindowsFormsApp1/PaintSuperForm.Designer.cs
+++ WindowsFormsApp1/PaintSuperForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsFormsApp1
 {
-    partial class Form1
+    partial class PaintSuperForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PaintSuperForm));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.Load_Button = new System.Windows.Forms.Button();
             this.Save_Button = new System.Windows.Forms.Button();

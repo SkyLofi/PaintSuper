3e5d80bd2f99cc5cba9b0021735b23c4a48f385c
Matěj Iljuchin <loupeznik@gmail.com>
2024-05-25 01:27:14 +0200

    added colors and loading mages

--- WindowsFormsApp1/PaintSuperForm.Designer.cs
+++ WindowsFormsApp1/PaintSuperForm.Designer.cs
@@ -35,7 +35,6 @@
             this.Pen_Tool_Button = new System.Windows.Forms.Button();
             this.Line_Tool_Button = new System.Windows.Forms.Button();
             this.Rectangle_Tool_Button = new System.Windows.Forms.Button();
-            this.Circle_Tool_Button = new System.Windows.Forms.Button();
             this.Ellipse_Tool_Button = new System.Windows.Forms.Button();
             this.Color_Button = new System.Windows.Forms.Button();
             this.ColorDialog = new System.Windows.Forms.ColorDialog();
@@ -133,22 +132,6 @@
             this.Rectangle_Tool_Button.UseVisualStyleBackColor = true;
             this.Rectangle_Tool_Button.Click += new System.EventHandler(this.Rectangle_Tool_Button_Click);
             // 
-            // Circle_Tool_Button
-            // 
-            this.Circle_Tool_Button.FlatAppearance.BorderColor = System.Drawing.Color.Black;
-            this.Circle_Tool_Button.FlatAppearance.BorderSize = 3;
-            this.Circle_Tool_Button.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.Circle_Tool_Button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
-            this.Circle_Tool_Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Circle_Tool_Button.Location = new System.Drawing.Point(344, 27);
-            this.Circle_Tool_Button.Name = "Circle_Tool_Button";
-            this.Circle_Tool_Button.Size = new System.Drawing.Size(55, 46);
-            this.Circle_Tool_Button.TabIndex = 6;
-            this.Circle_Tool_Button.Text = "Circle";
-            this.Circle_Tool_Button.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.Circle_Tool_Button.UseVisualStyleBackColor = true;
-            this.Circle_Tool_Button.Click += new System.EventHandler(this.Circle_Tool_Button_Click);
-            // 
             // Ellipse_Tool_Button
             // 
             this.Ellipse_Tool_Button.FlatAppearance.BorderColor = System.Drawing.Color.Black;
@@ -156,7 +139,7 @@
             this.Ellipse_Tool_Button.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.Ellipse_Tool_Button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
             this.Ellipse_Tool_Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Ellipse_Tool_Button.Location = new System.Drawing.Point(405, 27);
+            this.Ellipse_Tool_Button.Location = new System.Drawing.Point(344, 27);
             this.Ellipse_Tool_Button.Name = "Ellipse_Tool_Button";
             this.Ellipse_Tool_Button.Size = new System.Drawing.Size(55, 46);
             this.Ellipse_Tool_Button.TabIndex = 7;
@@ -172,7 +155,7 @@
             this.Color_Button.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.Color_Button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
             this.Color_Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Color_Button.Location = new System.Drawing.Point(466, 27);
+            this.Color_Button.Location = new System.Drawing.Point(405, 27);
             this.Color_Button.Name = "Color_Button";
             this.Color_Button.Size = new System.Drawing.Size(55, 46);
             this.Color_Button.TabIndex = 8;
@@ -187,7 +170,7 @@
             this.Eraser_Button.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.Eraser_Button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
             this.Eraser_Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Eraser_Button.Location = new System.Drawing.Point(527, 27);
+            this.Eraser_Button.Location = new System.Drawing.Point(466, 27);
             this.Eraser_Button.Name = "Eraser_Button";
             this.Eraser_Button.Size = new System.Drawing.Size(55, 46);
             this.Eraser_Button.TabIndex = 9;
@@ -204,7 +187,6 @@
             this.Controls.Add(this.Eraser_Button);
             this.Controls.Add(this.Color_Button);
             this.Controls.Add(this.Ellipse_Tool_Button);
-            this.Controls.Add(this.Circle_Tool_Button);
             this.Controls.Add(this.Rectangle_Tool_Button);
             this.Controls.Add(this.Line_Tool_Button);
             this.Controls.Add(this.Pen_Tool_Button);
@@ -228,7 +210,6 @@
         private System.Windows.Forms.Button Pen_Tool_Button;
         private System.Windows.Forms.Button Line_Tool_Button;
         private System.Windows.Forms.Button Rectangle_Tool_Button;
-        private System.Windows.Forms.Button Circle_Tool_Button;
         private System.Windows.Forms.Button Ellipse_Tool_Button;
         private System.Windows.Forms.Button Color_Button;
         private System.Windows.Forms.ColorDialog ColorDialog;
--- WindowsFormsApp1/PaintSuperForm.cs
+++ WindowsFormsApp1/PaintSuperForm.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp1
@@ -21,6 +15,7 @@ namespace WindowsFormsApp1
             line,
             eraser
         }
+        ColorDialog colorDialog = new ColorDialog();
         Color Barvicka = Color.Black;
         int BrushSize = 1;
         Drawingtool drawingtool;
@@ -32,34 +27,38 @@ namespace WindowsFormsApp1
         Pen pen;
         Pen eraser = new Pen(Color.White, 50);
         int x, y, sX, sY, cX, cY;
-
-
+        OpenFileDialog openFileDialog;
+        SaveFileDialog saveFileDialog;
         public PaintSuperForm()
         {
             InitializeComponent();
-
+            InitializeCustomComponents();
+        }
+        private void InitializeCustomComponents()
+        {
             this.Width = 900;
             this.Height = 600;
+
             bm = new Bitmap(pictureBox1.Width, pictureBox1.Height);
             g = Graphics.FromImage(bm);
             g.Clear(Color.White);
             pictureBox1.Image = bm;
 
-
+            openFileDialog = new OpenFileDialog();
+            saveFileDialog = new SaveFileDialog();
         }
-        
         private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
         {
             drawing = true;
             py = e.Location;
 
-            cX = e.X; 
+            cX = e.X;
             cY = e.Y;
         }
         private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
         {
             pen = new Pen(Barvicka, BrushSize);
-            if(drawing == true)
+            if (drawing == true)
             {
 
                 switch (drawingtool)
@@ -68,16 +67,15 @@ namespace WindowsFormsApp1
                         px = e.Location;
                         g.DrawLine(pen, px, py);
                         py = px;
-                    break;
+                        break;
 
                     case Drawingtool.eraser:
                         px = e.Location;
                         g.DrawLine(eraser, px, py);
                         py = px;
-                    break;
+                        break;
                 }
             }
-            
             pictureBox1.Refresh();
 
             x = e.X;
@@ -89,11 +87,11 @@ namespace WindowsFormsApp1
         private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
         {
             drawing = false;
-
+            pen = new Pen(Barvicka, BrushSize);
             sX = x - cX;
             sY = y - cY;
 
-            switch(drawingtool)
+            switch (drawingtool)
             {
                 case Drawingtool.ellipse:
                     g.DrawEllipse(pen, cX, cY, sX, sY);
@@ -104,64 +102,52 @@ namespace WindowsFormsApp1
                     break;
 
                 case Drawingtool.line:
-                    g.DrawLine(pen, cX, cY, x, y); 
+                    g.DrawLine(pen, cX, cY, x, y);
                     break;
             }
         }
         private void Save_Button_Click(object sender, EventArgs e)
         {
+            if (pictureBox1.Image != null)
+            {
+                saveFileDialog.Filter = "JPEG Files|*.jpg;*.jpeg|All Files|*.*";
+                saveFileDialog.FileName = "image.jpg";
 
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    bm.Save(saveFileDialog.FileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                }
+            }
         }
-
         private void Load_Button_Click(object sender, EventArgs e)
         {
+            openFileDialog.Filter = "JPEG Files|*.jpg;*.jpeg|All Files|*.*";
 
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                bm = new Bitmap(openFileDialog.FileName);
+                pictureBox1.Image = bm;
+            }
         }
-
-        private void Line_Tool_Button_Click(object sender, EventArgs e)
-        {
-            drawingtool = Drawingtool.line;
-        }
-        private void Pen_Tool_Button_Click(object sender, EventArgs e)
-        {
-            drawingtool = Drawingtool.pen;
-        }
-        private void Circle_Tool_Button_Click(object sender, EventArgs e)
-        {
-            drawingtool = Drawingtool.circle;
-        }
-
-        private void Rectangle_Tool_Button_Click(object sender, EventArgs e)
-        {
-            drawingtool = Drawingtool.rectangle;
-        }
-
-        private void Ellipse_Tool_Button_Click(object sender, EventArgs e)
-        {
-            drawingtool = Drawingtool.ellipse;
-        }
-
+        private void Line_Tool_Button_Click(object sender, EventArgs e) => drawingtool = Drawingtool.line;
+        private void Pen_Tool_Button_Click(object sender, EventArgs e) => drawingtool = Drawingtool.pen;
+        private void Rectangle_Tool_Button_Click(object sender, EventArgs e) => drawingtool = Drawingtool.rectangle;
+        private void Ellipse_Tool_Button_Click(object sender, EventArgs e) => drawingtool = Drawingtool.ellipse;
         private void Color_Button_Click(object sender, EventArgs e)
         {
-
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                Barvicka = colorDialog.Color;
+            }
         }
-
         private void PaintSuperForm_Load(object sender, EventArgs e)
         {
 
         }
-
-        private void Eraser_Button_Click(object sender, EventArgs e)
-        {
-            drawingtool = Drawingtool.eraser;
-
-        }
-
+        private void Eraser_Button_Click(object sender, EventArgs e) => drawingtool = Drawingtool.eraser;
         private void pictureBox1_Click(object sender, EventArgs e)
         {
 
         }
-
-        
     }
 }
